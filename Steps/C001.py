c16ab96471d013c65c2614545d962721211714c5
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-10-10 10:07:40 +0400

    10th Commit

--- Steps/C001.py
+++ Steps/C001.py
@@ -6,21 +6,31 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
+
 class C001():
-    def __init__(self, url, email, password):
+    def __init__(self, url, email, password, collection_txt):
 
         ''' --- Initialize URL, Email, Password --- '''
         self.url = 'https://' + url
         self.email = email
         self.password = password
+        self.collection_txt = collection_txt
 
     def startLogin(self):
 
         pTxt = "\n-------- Step 'C001' started!!! --------------------------------------------------------------------"
         print(pTxt)
 
-        self.driver = webdriver.Chrome()
-        #driver = webdriver.Chrome(os.getcwd() + '/WebDriver/chromedriver.exe')
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument("--test-type")
+        options.add_argument('--start-maximized')
+        # options.add_argument('--disable-java')
+        # options.add_argument('--incognito')
+        options.add_argument('--use-mock-keychain')
+        # self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(chrome_options=options)
+        # driver = webdriver.Chrome(os.getcwd() + '/WebDriver/chromedriver.exe')
         self.driver.maximize_window()
 
         ''' 1. Navigate to staging.getkumbu.com '''
@@ -31,8 +41,9 @@ class C001():
             pTxt = "\t\t(Success)\tLoad webpage successfully"
             print(pTxt)
         except:
-            pTxt = "\t\t(Failure)\tFailed to load webpage"
+            pTxt = "\t\t(Error)\tFailed to load webpage"
             print(pTxt)
+            self.driver.close()
             self.driver.quit()
             return
 
@@ -45,8 +56,9 @@ class C001():
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR, "input.kumbu-input"))
             )
         except:
-            pTxt = "\t\t(Failure)\tCan't find 'Email' and 'Password' Inputs"
+            pTxt = "\t\t(Error)\tCan't find 'Email' and 'Password' Inputs"
             print(pTxt)
+            self.driver.close()
             self.driver.quit()
             return
 
@@ -56,8 +68,9 @@ class C001():
             pTxt = "\t\t(Success)\tInput email successfully"
             print(pTxt)
         except:
-            pTxt = "\t\t(Failure)\tFailed to input 'email'"
+            pTxt = "\t\t(Error)\tFailed to input 'email'"
             print(pTxt)
+            self.driver.close()
             self.driver.quit()
             return
 
@@ -71,8 +84,9 @@ class C001():
             pTxt = "\t\t(Success)\tInputted 'Password' successfully"
             print(pTxt)
         except:
-            pTxt = "\t\t(Failure)\tFailed to input 'Password'"
+            pTxt = "\t\t(Error)\tFailed to input 'Password'"
             print(pTxt)
+            self.driver.close()
             self.driver.quit()
             return
 
@@ -87,8 +101,9 @@ class C001():
             pTxt = "\t\t(Success)\tClicked 'Sign in' button. Logged in successfully"
             print(pTxt)
         except:
-            pTxt = "\t\t(Failure)\tFailed to click 'Sign in' button"
+            pTxt = "\t\t(Error)\tFailed to click 'Sign in' button"
             print(pTxt)
+            self.driver.close()
             self.driver.quit()
             return
 
@@ -104,8 +119,9 @@ class C001():
             pTxt = "\t\t(Success)\tClicked 'New Collection'"
             print(pTxt)
         except:
-            pTxt = "\t\t(Failure)\tCan't click'New Collection'"
+            pTxt = "\t\t(Error)\tCan't click'New Collection'"
             print(pTxt)
+            self.driver.close()
             self.driver.quit()
             return
 
@@ -118,16 +134,17 @@ class C001():
             )
 
             actionChains = ActionChains(self.driver)
-            actionChains.click(new_collection).key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL)\
+            actionChains.click(new_collection).key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL) \
                 .send_keys(Keys.DELETE).perform()
 
-            new_collection.send_keys("Kumbu Test 4")
+            new_collection.send_keys("Kumbu Test 5")
 
             pTxt = "\t\t(Success)\t"
             print(pTxt)
         except:
-            pTxt = "\t\t(Failure)\t"
+            pTxt = "\t\t(Error)\t"
             print(pTxt)
+            self.driver.close()
             self.driver.quit()
             return
 
@@ -143,17 +160,65 @@ class C001():
             actionChains.click(new_collection).key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL) \
                 .send_keys(Keys.DELETE).perform()
 
-            new_collection.send_keys("Kumbu Test 4")
+            new_collection.send_keys(self.collection_txt)
 
-            pTxt = "\t\t(Success)\t"
+            display_txt = self.driver.find_element_by_css_selector("p.collection-meta.item-info").text.strip()
+            pTxt = "\t\t(Success)\t'{}' is displayed".format(display_txt)
             print(pTxt)
         except:
-            pTxt = "\t\t(Failure)\t"
+            pTxt = "\t\t(Error)\t"
             print(pTxt)
+            self.driver.close()
             self.driver.quit()
             return
 
+        ''' 8. Click 'Back to collections' '''
+        pTxt = "\n8. Click 'Back to collections'\n"
+        print(pTxt)
+        try:
+            back_btn = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "a.back-collections"))
+            )
+            back_btn.click()
+            pTxt = "\t\t(Success)\t Clicked 'Back to collections'"
+            print(pTxt)
+        except:
+            pTxt = "\t\t(Error)\t Failed to click 'Back to collections'"
+            print(pTxt)
+            self.driver.close()
+            self.driver.quit()
+            return
+
+        ''' 9. Check that 'Collection for Test $TEST_NUMBER' is visible on the page '''
+        pTxt = "\n9. Check that 'Collection for Test $TEST_NUMBER' is visible on the page\n"
+        print(pTxt)
+        try:
+            collections = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "div.collection.columns.small-12.medium-3.text-center"))
+            )
+            flag = False
+            for collection in collections:
+                if self.collection_txt in collection.text.strip():
+                    flag = True
+                    break
+
+            if flag:
+                pTxt = "\t\t(Success)\t '{}' is displayed".format(self.collection_txt)
+            else:
+                pTxt = "\t\t(Failure)\t '{}' is not displayed".format(self.collection_txt)
+            print(pTxt)
+        except:
+            pTxt = "\t\t(Error)\t '{}' is not displayed".format(self.collection_txt)
+            print(pTxt)
+            self.driver.close()
+            self.driver.quit()
+            return
+
+        self.driver.close()
+        self.driver.quit()
+
 
 if __name__ == '__main__':
-    app = C001(url='staging.getkumbu.com', email='kumbutest@mailinator.com', password='kumbu is cool')
+    app = C001(url='staging.getkumbu.com', email='kumbutest@mailinator.com', password='kumbu is cool',
+               collection_txt='Kumbu Test 4')
     app.startLogin()
